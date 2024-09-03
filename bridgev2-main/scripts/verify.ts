07649d5677f0f8293cdc4a2ff8b71664c022ef05
Kd526 <davisthoyah@gmail.com>
2024-09-03 09:31:05 +0300

    tests scripts

--- bridgev2-main/scripts/verify.ts
+++ bridgev2-main/scripts/verify.ts
@@ -2,13 +2,13 @@ import {run} from "hardhat";
 
 async function main() {
     //deployed contract address
-    const contractAddress = "0x980B2F387BBECD67d94B2b6Eebd4FD238946466a";
+    const contractAddress = "0x67e0B3f4069e59812EecC65DF127811A43AF5Eb9";
   
     // constructor arguments
     const constructorArguments = [
       "0x6EDCE65403992e310A62460808c4b910D972f10f",
       "0x25F0105CBca79C300Efe203503AF091c0dfF1FC3",
-      "ARB"
+      "ETH"
     ];
   
     try {
